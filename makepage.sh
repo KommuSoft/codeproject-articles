489b6dac50b94a195611147694aa25d633275b21
KommuSoft <vanonsem.willem@gmail.com>
2014-11-20 21:19:52 +1100

    make fix

--- makepage.sh
+++ makepage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 trgt=$(basename $1 ".md")
-cat "template/header.htm" <$(pandoc -t htm $1) "footer.htm" > "$trgt.htm"
+cat "template/header.htm" <(pandoc -t html $1) "template/footer.htm" > "$trgt.htm"
